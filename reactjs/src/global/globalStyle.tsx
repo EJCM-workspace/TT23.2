73bb651a5c87465a0081631f817100f0cb81ac44
Iago <iagocesarts@hotmail.com>
2023-08-12 16:54:38 -0300

    feature: Adição dos assets e global.

---
+++ reactjs/src/global/globalStyle.tsx
@@ -0,0 +1,11 @@
+export const global = {
+    colors: {
+        branco: '#FFFFFF',
+        laranja: '#FF420F',
+        marromEscuro:'#693F00',
+        bege:'#C99A54',
+        offWhite:'#FEF2E4',
+        cinza:'#AAAAAA',
+        cinzaEscuro: '#262626'
+    }
+}
